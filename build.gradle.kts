3ee8d11d8d6149658dee7fb8765aabee6f70cc7a
Tim Mwangi <tim@traceable.ai>
2020-07-12 15:19:38 -0700

    chore: update to Apache 2.0 License

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ import org.hypertrace.gradle.publishing.License
 plugins {
   id("org.hypertrace.repository-plugin") version "0.1.2"
   id("org.hypertrace.ci-utils-plugin") version "0.1.1"
-  id("org.hypertrace.publish-plugin") version "0.1.3" apply false
+  id("org.hypertrace.publish-plugin") version "0.3.0" apply false
 }
 
 subprojects {
   group = "org.hypertrace.core.documentstore"
   pluginManager.withPlugin("org.hypertrace.publish-plugin") {
     configure<HypertracePublishExtension> {
-      license.set(License.AGPL_V3)
+      license.set(License.APACHE_2_0)
     }
   }
 }
