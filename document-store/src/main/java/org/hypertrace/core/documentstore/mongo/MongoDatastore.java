a23300bdfee2cdfd0d88201f3210d369c7c389cb
Buchi Reddy Busi Reddy <48895538+buchi-busireddy@users.noreply.github.com>
2020-11-17 22:51:21 -0800

    Upgrading Mongo java driver from 3.12 to 4.1.1 (#12)
    Upgrading Mongo java driver to 4.1.1 from 3.12

--- document-store/src/main/java/org/hypertrace/core/documentstore/mongo/MongoDatastore.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/mongo/MongoDatastore.java
@@ -1,79 +1,89 @@
 package org.hypertrace.core.documentstore.mongo;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.BasicDBObject;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCommandException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import com.typesafe.config.Config;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.bson.Document;
 import org.hypertrace.core.documentstore.Collection;
 import org.hypertrace.core.documentstore.Datastore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MongoDatastore implements Datastore {
+  private static final Logger LOGGER = LoggerFactory.getLogger(MongoDatastore.class);
 
   private static final String DEFAULT_DB_NAME = "default_db";
   private MongoClient client;
+  private MongoDatabase database;
 
   @Override
   public boolean init(Config config) {
-    try {
-      if (config.hasPath("url")) {
-        client = new MongoClient(new MongoClientURI(config.getString("url")));
-      } else {
-        String hostName = config.getString("host");
-        int port = config.getInt("port");
-        client = new MongoClient(hostName, port);
-      }
-    } catch (IllegalArgumentException e) {
-        throw new IllegalArgumentException(
-            String.format("Unable to instantiate MongoClient with config:%s",config), e);
+    ConnectionString connString;
+    if (config.hasPath("url")) {
+      connString = new ConnectionString(config.getString("url"));
+    } else {
+      String hostName = config.getString("host");
+      int port = config.getInt("port");
+      connString = new ConnectionString("mongodb://" + hostName + ":" + port);
     }
+
+    MongoClientSettings settings = MongoClientSettings.builder()
+        .applyConnectionString(connString)
+        .retryWrites(true)
+        .build();
+    client = MongoClients.create(settings);
+
+    database = client.getDatabase(DEFAULT_DB_NAME);
     return true;
   }
 
   @Override
   public Set<String> listCollections() {
     Set<String> collections = new HashSet<>();
-    List<String> databaseNames = client.getDatabaseNames();
-    for (String dbName : databaseNames) {
-      Set<String> collectionNames = client.getDB(dbName).getCollectionNames();
-      for (String collectionName : collectionNames) {
-        collections.add(dbName + "." + collectionName);
-      }
+    for (String collectionName : database.listCollectionNames()) {
+      collections.add(database.getName() + "." + collectionName);
     }
     return collections;
   }
 
   @Override
   public boolean createCollection(String collectionName, Map<String, String> options) {
-    DBObject dbObject = null;
-    DBCollection collection =
-        client.getDB(DEFAULT_DB_NAME).createCollection(collectionName, dbObject);
-
-    return collection != null;
-  }
-
-  @Override
-  public boolean deleteCollection(String collectionName) {
-    DBCollection collection = client.getDB(DEFAULT_DB_NAME).getCollection(collectionName);
-    if (collection != null) {
-      collection.drop();
+    try {
+      database.createCollection(collectionName);
+    } catch (MongoCommandException e) {
+      if (!"NamespaceExists".equals(e.getErrorCodeName())) {
+        LOGGER.error("Could not create collection: {}", collectionName, e);
+        return false;
+      }
     }
     return true;
   }
 
   @Override
+  public boolean deleteCollection(String collectionName) {
+    com.mongodb.client.MongoCollection<Document> collection = database.getCollection(collectionName);
+    collection.drop();
+    return true;
+  }
+
+  @Override
   public Collection getCollection(String collectionName) {
-    return new MongoCollection(client.getDB(DEFAULT_DB_NAME).getCollection(collectionName));
+    return new MongoCollection(database.getCollection(collectionName, BasicDBObject.class));
   }
 
   @Override
   public boolean healthCheck() {
-    return this.client.getDB(DEFAULT_DB_NAME).command("ping").ok();
+    Document document = this.database.runCommand(new Document("ping", "1"));
+    return !document.isEmpty();
   }
 
   @VisibleForTesting
