ccc3c99b131e3b1fa1d3e1b58f6f0dd4eb9869e1
SJ <shubhamjindal18@gmail.com>
2021-01-08 13:25:57 +0530

    feat: support projections in mongo

--- document-store/src/main/java/org/hypertrace/core/documentstore/Query.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/Query.java
@@ -6,11 +6,24 @@ import java.util.Objects;
 
 public class Query {
 
-  // support only filter for now. Add projections and aggregations and group by later.
+  // support only filter for now. Add aggregations and group by later.
+  private final List<String> selections = new ArrayList<>();
   private Filter filter;
-  private List<OrderBy> orderBys = new ArrayList<>();
+  private final List<OrderBy> orderBys = new ArrayList<>();
   private Integer offset;
   private Integer limit;
+
+  public void addAllSelections(List<String> selections) {
+    this.selections.addAll(selections);
+  }
+
+  public void addSelection(String selection) {
+    this.selections.add(selection);
+  }
+
+  public List<String> getSelections() {
+    return selections;
+  }
 
   public Filter getFilter() {
     return filter;
@@ -51,7 +64,8 @@ public class Query {
   @Override
   public String toString() {
     return "Query{" +
-        "filter=" + filter +
+        "selections=" + selections +
+        ", filter=" + filter +
         ", orderBys=" + orderBys +
         ", offset=" + offset +
         ", limit=" + limit +
@@ -60,14 +74,11 @@ public class Query {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
     Query query = (Query) o;
-    return Objects.equals(filter, query.filter) &&
+    return Objects.equals(selections, query.selections) &&
+        Objects.equals(filter, query.filter) &&
         Objects.equals(orderBys, query.orderBys) &&
         Objects.equals(offset, query.offset) &&
         Objects.equals(limit, query.limit);
@@ -75,6 +86,6 @@ public class Query {
 
   @Override
   public int hashCode() {
-    return Objects.hash(filter, orderBys, offset, limit);
+    return Objects.hash(selections, filter, orderBys, offset, limit);
   }
 }
--- document-store/src/test/java/org/hypertrace/core/documentstore/mongo/MongoCollectionTest.java
+++ document-store/src/test/java/org/hypertrace/core/documentstore/mongo/MongoCollectionTest.java
@@ -1,5 +1,6 @@
 package org.hypertrace.core.documentstore.mongo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
@@ -12,11 +13,14 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCursor;
 import java.util.List;
 import java.util.Map;
+
+import org.bson.conversions.Bson;
 import org.hypertrace.core.documentstore.Filter;
 import org.hypertrace.core.documentstore.Query;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 /** Unit tests for utility/helper methods in {@link MongoCollection} */
 public class MongoCollectionTest {
@@ -35,62 +39,62 @@ public class MongoCollectionTest {
     {
       Filter filter = new Filter(Filter.Op.EQ, "key1", "val1");
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals("val1", query.get("key1"));
+      assertEquals("val1", query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.GT, "key1", 5);
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(new BasicDBObject("$gt", 5), query.get("key1"));
+      assertEquals(new BasicDBObject("$gt", 5), query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.GTE, "key1", 5);
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(new BasicDBObject("$gte", 5), query.get("key1"));
+      assertEquals(new BasicDBObject("$gte", 5), query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.LT, "key1", 5);
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(new BasicDBObject("$lt", 5), query.get("key1"));
+      assertEquals(new BasicDBObject("$lt", 5), query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.LTE, "key1", 5);
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(new BasicDBObject("$lte", 5), query.get("key1"));
+      assertEquals(new BasicDBObject("$lte", 5), query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.EXISTS, "key1", null);
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(new BasicDBObject("$exists", true), query.get("key1"));
+      assertEquals(new BasicDBObject("$exists", true), query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.NOT_EXISTS, "key1", null);
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(new BasicDBObject("$exists", false), query.get("key1"));
+      assertEquals(new BasicDBObject("$exists", false), query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.LIKE, "key1", ".*abc");
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(
+      assertEquals(
           new BasicDBObject("$regex", ".*abc").append("$options", "i"), query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.IN, "key1", List.of("abc", "xyz"));
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(new BasicDBObject("$in", List.of("abc", "xyz")), query.get("key1"));
+      assertEquals(new BasicDBObject("$in", List.of("abc", "xyz")), query.get("key1"));
     }
 
     {
       Filter filter = new Filter(Filter.Op.CONTAINS, "key1", "abc");
       Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertEquals(new BasicDBObject("$elemMatch", "abc"), query.get("key1"));
+      assertEquals(new BasicDBObject("$elemMatch", "abc"), query.get("key1"));
     }
   }
 
@@ -129,7 +133,7 @@ public class MongoCollectionTest {
 
     Filter filter = filter1.or(filter2);
     Map<String, Object> query = mongoCollection.parseQuery(filter);
-    Assertions.assertEquals(2, ((List) query.get("$or")).size());
+    assertEquals(2, ((List) query.get("$or")).size());
     Assertions.assertTrue(
         ((List) ((Map) ((List) query.get("$or")).get(0)).get("$and"))
             .containsAll(List.of(Map.of("key1", "val1"), Map.of("key2", "val2"))));
@@ -222,5 +226,21 @@ public class MongoCollectionTest {
 
     mongoCollection.total(query);
     verify(collection, times(1)).countDocuments(any(BasicDBObject.class));
+  }
+
+  @Test
+  public void testProjections() {
+    Query query = new Query();
+    query.addAllSelections(List.of("proj1", "proj2"));
+
+    FindIterable findCursor = mock(FindIterable.class);
+    when(collection.find(any(BasicDBObject.class))).thenReturn(findCursor);
+
+    FindIterable projectionCursor = mock(FindIterable.class);
+    ArgumentCaptor<Bson> projectionArgumentCaptor = ArgumentCaptor.forClass(Bson.class);
+    when(findCursor.projection(projectionArgumentCaptor.capture())).thenReturn(projectionCursor);
+
+    mongoCollection.search(query);
+    assertEquals("{\"proj1\": 1, \"proj2\": 1}", projectionArgumentCaptor.getValue().toString());
   }
 }
