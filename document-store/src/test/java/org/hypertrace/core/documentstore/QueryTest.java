b70a7db2f82266eb9069da876ad83b3d004efbf8
SJ <shubhamjindal18@gmail.com>
2021-01-08 15:35:02 +0530

    added tests for Query

--- document-store/src/test/java/org/hypertrace/core/documentstore/QueryTest.java
+++ document-store/src/test/java/org/hypertrace/core/documentstore/QueryTest.java
@@ -50,4 +50,26 @@ public class QueryTest {
     orderBy.setIsAsc(true);
     Assertions.assertTrue(query1.equals(query2));
   }
+
+  @Test
+  public void testSelections() {
+    Query query = new Query();
+    Assertions.assertEquals(0, query.getSelections().size());
+
+    query.addSelection("selection1");
+    Assertions.assertEquals(1, query.getSelections().size());
+    Assertions.assertEquals("selection1", query.getSelections().get(0));
+
+    query.addSelection("selection2");
+    Assertions.assertEquals(2, query.getSelections().size());
+    Assertions.assertEquals("selection1", query.getSelections().get(0));
+    Assertions.assertEquals("selection2", query.getSelections().get(1));
+
+    query.addAllSelections(List.of("selection3", "selection4"));
+    Assertions.assertEquals(4, query.getSelections().size());
+    Assertions.assertEquals("selection1", query.getSelections().get(0));
+    Assertions.assertEquals("selection2", query.getSelections().get(1));
+    Assertions.assertEquals("selection3", query.getSelections().get(2));
+    Assertions.assertEquals("selection4", query.getSelections().get(3));
+  }
 }
