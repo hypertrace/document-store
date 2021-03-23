5e47ed9e571db89000e896e264a2a526fd46ac5f
kotharironak <53209990+kotharironak@users.noreply.github.com>
2021-03-23 10:12:27 +0530

    Update document-store/src/integrationTest/java/org/hypertrace/core/documentstore/DocStoreTest.java

--- document-store/src/integrationTest/java/org/hypertrace/core/documentstore/DocStoreTest.java
+++ document-store/src/integrationTest/java/org/hypertrace/core/documentstore/DocStoreTest.java
@@ -122,7 +122,6 @@ public class DocStoreTest {
   @MethodSource
   private static Stream<Arguments> databaseContextProvider() {
     return Stream.of(Arguments.of(MONGO_STORE), Arguments.of(POSTGRES_STORE));
-    // return Stream.of(Arguments.of(MONGO_STORE));
   }
 
   @ParameterizedTest
