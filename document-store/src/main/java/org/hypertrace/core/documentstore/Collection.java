5139be7fc2b18509e9f6ae7d6474fa30b7aa9c35
Ronak <ronak@traceable.ai>
2021-03-30 14:45:13 +0530

    chore: handles formatting issue

--- document-store/src/main/java/org/hypertrace/core/documentstore/Collection.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/Collection.java
@@ -103,8 +103,8 @@ public interface Collection {
   DocStoreResult create(Key key, Document document) throws IOException;
 
   /**
-   * Update an existing document if condition is evaluated to true. Condition will help in
-   * providing optimistic locking support for concurrency update.
+   * Update an existing document if condition is evaluated to true. Condition will help in providing
+   * optimistic locking support for concurrency update.
    *
    * @param key Unique key of the document in the collection.
    * @param document Document to be updated.
