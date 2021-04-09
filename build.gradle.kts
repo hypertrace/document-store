2cdb9d3a037bcba5e17f30dcc45ec7a588420b2f
ravisingal <62086374+ravisingal@users.noreply.github.com>
2021-04-09 12:01:58 +0530

    publish artifacts to hypertrace artifactory (#46)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.hypertrace.gradle.publishing.HypertracePublishExtension
 import org.hypertrace.gradle.publishing.License
 
 plugins {
-  id("org.hypertrace.repository-plugin") version "0.1.2"
-  id("org.hypertrace.ci-utils-plugin") version "0.1.1"
-  id("org.hypertrace.publish-plugin") version "0.3.0" apply false
-  id("org.hypertrace.code-style-plugin") version "1.0.0" apply false
+  id("org.hypertrace.repository-plugin") version "0.4.0"
+  id("org.hypertrace.ci-utils-plugin") version "0.3.0"
+  id("org.hypertrace.publish-plugin") version "1.0.2" apply false
+  id("org.hypertrace.code-style-plugin") version "1.1.0" apply false
 }
 
 subprojects {
