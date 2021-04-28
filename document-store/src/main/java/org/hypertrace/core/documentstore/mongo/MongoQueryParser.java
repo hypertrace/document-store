f4cd817e95d63cadfce2b465f7ecff01034743eb
GurtejSohi <gurtejsohi25@gmail.com>
2021-04-28 12:40:21 +0530

    refactor: extract mongo query parsing methods into MongoQueryParser

---
+++ document-store/src/main/java/org/hypertrace/core/documentstore/mongo/MongoQueryParser.java
@@ -0,0 +1,103 @@
+package org.hypertrace.core.documentstore.mongo;
+
+import static org.hypertrace.core.documentstore.Collection.UNSUPPORTED_QUERY_OPERATION;
+
+import com.mongodb.BasicDBObject;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.hypertrace.core.documentstore.Filter;
+import org.hypertrace.core.documentstore.OrderBy;
+
+public class MongoQueryParser {
+
+  static Map<String, Object> parseFilter(Filter filter) {
+    if (filter.isComposite()) {
+      Filter.Op op = filter.getOp();
+      switch (op) {
+        case OR:
+        case AND:
+          {
+            List<Map<String, Object>> childMapList =
+                Arrays.stream(filter.getChildFilters())
+                    .map(MongoQueryParser::parseFilter)
+                    .filter(map -> !map.isEmpty())
+                    .collect(Collectors.toList());
+            if (!childMapList.isEmpty()) {
+              return Map.of("$" + op.name().toLowerCase(), childMapList);
+            } else {
+              return Collections.emptyMap();
+            }
+          }
+        default:
+          throw new UnsupportedOperationException(
+              String.format("Boolean operation:%s not supported", op));
+      }
+    } else {
+      Filter.Op op = filter.getOp();
+      Object value = filter.getValue();
+      Map<String, Object> map = new HashMap<>();
+      switch (op) {
+        case EQ:
+          map.put(filter.getFieldName(), value);
+          break;
+        case LIKE:
+          // Case insensitive regex search
+          map.put(
+              filter.getFieldName(), new BasicDBObject("$regex", value).append("$options", "i"));
+          break;
+        case NOT_IN:
+          map.put(filter.getFieldName(), new BasicDBObject("$nin", value));
+          break;
+        case IN:
+          map.put(filter.getFieldName(), new BasicDBObject("$in", value));
+          break;
+        case CONTAINS:
+          map.put(filter.getFieldName(), new BasicDBObject("$elemMatch", filter.getValue()));
+          break;
+        case GT:
+          map.put(filter.getFieldName(), new BasicDBObject("$gt", value));
+          break;
+        case LT:
+          map.put(filter.getFieldName(), new BasicDBObject("$lt", value));
+          break;
+        case GTE:
+          map.put(filter.getFieldName(), new BasicDBObject("$gte", value));
+          break;
+        case LTE:
+          map.put(filter.getFieldName(), new BasicDBObject("$lte", value));
+          break;
+        case EXISTS:
+          map.put(filter.getFieldName(), new BasicDBObject("$exists", true));
+          break;
+        case NOT_EXISTS:
+          map.put(filter.getFieldName(), new BasicDBObject("$exists", false));
+          break;
+        case NEQ:
+          // $ne operator in Mongo also returns the results, where the key does not exist in the
+          // document. This is as per semantics of EQ vs NEQ. So, if you need documents where
+          // key exists, consumer needs to add additional filter.
+          // https://github.com/hypertrace/document-store/pull/20#discussion_r547101520
+          map.put(filter.getFieldName(), new BasicDBObject("$ne", value));
+          break;
+        case AND:
+        case OR:
+        default:
+          throw new UnsupportedOperationException(UNSUPPORTED_QUERY_OPERATION);
+      }
+      return map;
+    }
+  }
+
+  static LinkedHashMap<String, Object> parseOrderBys(List<OrderBy> orderBys) {
+    LinkedHashMap<String, Object> orderByMap = new LinkedHashMap<>();
+    for (OrderBy orderBy : orderBys) {
+      orderByMap.put(orderBy.getField(), (orderBy.isAsc() ? 1 : -1));
+    }
+    return orderByMap;
+  }
+}
--- document-store/src/test/java/org/hypertrace/core/documentstore/mongo/MongoCollectionTest.java
+++ document-store/src/test/java/org/hypertrace/core/documentstore/mongo/MongoCollectionTest.java
@@ -12,13 +12,9 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCursor;
 import java.util.List;
-import java.util.Map;
 import org.bson.conversions.Bson;
 import org.hypertrace.core.documentstore.Filter;
-import org.hypertrace.core.documentstore.Filter.Op;
-import org.hypertrace.core.documentstore.OrderBy;
 import org.hypertrace.core.documentstore.Query;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -33,135 +29,6 @@ public class MongoCollectionTest {
   public void setup() {
     collection = mock(com.mongodb.client.MongoCollection.class);
     mongoCollection = new MongoCollection(collection);
-  }
-
-  @Test
-  public void testParseSimpleQuery() {
-    {
-      Filter filter = new Filter(Filter.Op.EQ, "key1", "val1");
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals("val1", query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.GT, "key1", 5);
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$gt", 5), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.GTE, "key1", 5);
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$gte", 5), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.LT, "key1", 5);
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$lt", 5), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.LTE, "key1", 5);
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$lte", 5), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.EXISTS, "key1", null);
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$exists", true), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.NOT_EXISTS, "key1", null);
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$exists", false), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.LIKE, "key1", ".*abc");
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$regex", ".*abc").append("$options", "i"), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.IN, "key1", List.of("abc", "xyz"));
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$in", List.of("abc", "xyz")), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Op.NOT_IN, "key1", List.of("abc", "xyz"));
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$nin", List.of("abc", "xyz")), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.CONTAINS, "key1", "abc");
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      assertEquals(new BasicDBObject("$elemMatch", "abc"), query.get("key1"));
-    }
-
-    {
-      Filter filter = new Filter(Filter.Op.NEQ, "key1", "abc");
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      BasicDBObject notEquals = new BasicDBObject();
-      notEquals.append("$ne", "abc");
-      Assertions.assertEquals(notEquals, query.get("key1"));
-    }
-  }
-
-  @Test
-  public void testParseAndOrQuery() {
-    {
-      Filter filter =
-          new Filter(Filter.Op.EQ, "key1", "val1").and(new Filter(Filter.Op.EQ, "key2", "val2"));
-
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertTrue(query.get("$and") instanceof List);
-      Assertions.assertTrue(
-          ((List) query.get("$and"))
-              .containsAll(List.of(Map.of("key1", "val1"), Map.of("key2", "val2"))));
-    }
-
-    {
-      Filter filter =
-          new Filter(Filter.Op.EQ, "key1", "val1").or(new Filter(Filter.Op.EQ, "key2", "val2"));
-
-      Map<String, Object> query = mongoCollection.parseQuery(filter);
-      Assertions.assertTrue(query.get("$or") instanceof List);
-      Assertions.assertTrue(
-          ((List) query.get("$or"))
-              .containsAll(List.of(Map.of("key1", "val1"), Map.of("key2", "val2"))));
-    }
-  }
-
-  @Test
-  public void testParseNestedQuery() {
-    Filter filter1 =
-        new Filter(Filter.Op.EQ, "key1", "val1").and(new Filter(Filter.Op.EQ, "key2", "val2"));
-
-    Filter filter2 =
-        new Filter(Filter.Op.EQ, "key3", "val3").and(new Filter(Filter.Op.EQ, "key4", "val4"));
-
-    Filter filter = filter1.or(filter2);
-    Map<String, Object> query = mongoCollection.parseQuery(filter);
-    assertEquals(2, ((List) query.get("$or")).size());
-    Assertions.assertTrue(
-        ((List) ((Map) ((List) query.get("$or")).get(0)).get("$and"))
-            .containsAll(List.of(Map.of("key1", "val1"), Map.of("key2", "val2"))));
-    Assertions.assertTrue(
-        ((List) ((Map) ((List) query.get("$or")).get(1)).get("$and"))
-            .containsAll(List.of(Map.of("key3", "val3"), Map.of("key4", "val4"))));
-  }
-
-  @Test
-  public void testParseOrderByQuery() {
-    List<OrderBy> orderBys =
-        List.of(new OrderBy("key1", true), new OrderBy("key2", false), new OrderBy("key3", true));
-    assertEquals(
-        Map.of("key1", 1, "key2", -1, "key3", 1), mongoCollection.parseOrderByQuery(orderBys));
   }
 
   @Test
