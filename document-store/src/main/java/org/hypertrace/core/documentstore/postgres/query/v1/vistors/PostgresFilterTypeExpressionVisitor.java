213eff2c1df39a4d8188b4893d9cffa03c664a31
Ronak <ronak@traceable.ai>
2022-07-05 10:33:01 +0530

    feat: add support for logical expression for new query api

--- document-store/src/main/java/org/hypertrace/core/documentstore/postgres/query/v1/vistors/PostgresFilterTypeExpressionVisitor.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/postgres/query/v1/vistors/PostgresFilterTypeExpressionVisitor.java
@@ -1,7 +1,11 @@
 package org.hypertrace.core.documentstore.postgres.query.v1.vistors;
 
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.hypertrace.core.documentstore.expression.impl.LogicalExpression;
 import org.hypertrace.core.documentstore.expression.impl.RelationalExpression;
+import org.hypertrace.core.documentstore.expression.operators.LogicalOperator;
 import org.hypertrace.core.documentstore.expression.operators.RelationalOperator;
 import org.hypertrace.core.documentstore.expression.type.SelectTypeExpression;
 import org.hypertrace.core.documentstore.parser.FilterTypeExpressionVisitor;
@@ -17,8 +21,16 @@ public class PostgresFilterTypeExpressionVisitor implements FilterTypeExpression
   }
 
   @Override
-  public <T> T visit(final LogicalExpression expression) {
-    throw new UnsupportedOperationException("Not yet supported logical expression");
+  public String visit(final LogicalExpression expression) {
+    Collector<String, ?, String> collector =
+        getCollectorForLogicalOperator(expression.getOperator());
+    String childList =
+        expression.getOperands().stream()
+            .map(exp -> exp.accept(this))
+            .filter(str -> !StringUtils.isEmpty((String) str))
+            .map(str -> "(" + str + ")")
+            .collect(collector);
+    return !childList.isEmpty() ? childList : null;
   }
 
   @Override
@@ -36,5 +48,15 @@ public class PostgresFilterTypeExpressionVisitor implements FilterTypeExpression
 
     return PostgresUtils.parseNonCompositeFilter(
         key, operator.toString(), value, this.postgresQueryParser.getParamsBuilder());
+  }
+
+  private Collector getCollectorForLogicalOperator(LogicalOperator operator) {
+    if (operator.equals(LogicalOperator.OR)) {
+      return Collectors.joining(" OR ");
+    } else if (operator.equals(LogicalOperator.AND)) {
+      return Collectors.joining(" AND ");
+    }
+    throw new UnsupportedOperationException(
+        String.format("Query operation:%s not supported", operator));
   }
 }
