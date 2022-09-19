a729727b767f6c2c1ba7108c11c66a28d92656a3
SrikarMannepalli <37926341+SrikarMannepalli@users.noreply.github.com>
2022-09-19 15:42:55 +0530

    rename DISTINT to DISTINCT_ARRAY (#128)

--- document-store/src/main/java/org/hypertrace/core/documentstore/mongo/parser/MongoAggregateExpressionParser.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/mongo/parser/MongoAggregateExpressionParser.java
@@ -3,7 +3,7 @@ package org.hypertrace.core.documentstore.mongo.parser;
 import static java.util.Collections.unmodifiableMap;
 import static org.hypertrace.core.documentstore.expression.operators.AggregationOperator.AVG;
 import static org.hypertrace.core.documentstore.expression.operators.AggregationOperator.COUNT;
-import static org.hypertrace.core.documentstore.expression.operators.AggregationOperator.DISTINCT;
+import static org.hypertrace.core.documentstore.expression.operators.AggregationOperator.DISTINCT_ARRAY;
 import static org.hypertrace.core.documentstore.expression.operators.AggregationOperator.MAX;
 import static org.hypertrace.core.documentstore.expression.operators.AggregationOperator.MIN;
 import static org.hypertrace.core.documentstore.expression.operators.AggregationOperator.SUM;
@@ -23,7 +23,7 @@ final class MongoAggregateExpressionParser extends MongoSelectTypeExpressionPars
           new EnumMap<>(AggregationOperator.class) {
             {
               put(AVG, "$avg");
-              put(DISTINCT, "$addToSet");
+              put(DISTINCT_ARRAY, "$addToSet");
               put(SUM, "$sum");
               put(MIN, "$min");
               put(MAX, "$max");
