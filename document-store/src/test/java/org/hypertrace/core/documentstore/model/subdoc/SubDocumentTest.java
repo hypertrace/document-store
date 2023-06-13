1a7928feaa16477245e08b485cc988a91f17c28f
shantanu.bhosale <shantanu.bhosale@traceable.ai>
2023-06-13 11:27:55 +0530

    Fix unit test

--- document-store/src/test/java/org/hypertrace/core/documentstore/model/subdoc/SubDocumentTest.java
+++ document-store/src/test/java/org/hypertrace/core/documentstore/model/subdoc/SubDocumentTest.java
@@ -3,53 +3,55 @@ package org.hypertrace.core.documentstore.model.subdoc;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SubDocumentTest {
-  @Test
-  void testValidSubDocumentPaths() {
-    String alphabetsValidPath = "price";
-    String numbersValidPath = "1234567890";
-    String stringSeparatedByDotValidPath = "price.quantity";
-    String uuidValidPath = "67aa7623-fc40-4112-b958-cbd6d4c077b1";
-    String createdTimeImplicitValidPath = "createdTime";
-    String lastUpdatedTimeImplicitValidPath = "lastUpdatedTime";
-    SubDocument subDocumentForAlphabetsValidPath =
-        SubDocument.builder().path(alphabetsValidPath).build();
-    assertEquals(alphabetsValidPath, subDocumentForAlphabetsValidPath.getPath());
-    SubDocument subDocumentForNumbersValidPath =
-        SubDocument.builder().path(numbersValidPath).build();
-    assertEquals(numbersValidPath, subDocumentForNumbersValidPath.getPath());
-    SubDocument subDocumentForDotValidPath =
-        SubDocument.builder().path(stringSeparatedByDotValidPath).build();
-    assertEquals(stringSeparatedByDotValidPath, subDocumentForDotValidPath.getPath());
-    SubDocument subDocumentForUuidValidPath = SubDocument.builder().path(uuidValidPath).build();
-    assertEquals(uuidValidPath, subDocumentForUuidValidPath.getPath());
-    assertEquals(createdTimeImplicitValidPath, SubDocument.implicitCreatedTime().getPath());
-    assertEquals(lastUpdatedTimeImplicitValidPath, SubDocument.implicitUpdatedTime().getPath());
+
+  @MethodSource
+  private static Stream<Arguments> validPathsProvider() {
+    return Stream.of(
+        Arguments.of("price"),
+        Arguments.of("1234567890"),
+        Arguments.of("price.quantity"),
+        Arguments.of("67aa7623-fc40-4112-b958-cbd6d4c077b1"),
+        Arguments.of("first_name"));
+  }
+
+  @MethodSource
+  private static Stream<Arguments> inValidPathsProvider() {
+    return Stream.of(
+        Arguments.of("price::quantity"),
+        Arguments.of("{hello_world"),
+        Arguments.of("hello_world}"),
+        Arguments.of("createdTime"),
+        Arguments.of("lastUpdatedTime"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("validPathsProvider")
+  void testValidSubDocumentPaths(String validPath) {
+    SubDocument subDocumentForValidPath = SubDocument.builder().path(validPath).build();
+    assertEquals(validPath, subDocumentForValidPath.getPath());
+  }
+
+  @ParameterizedTest
+  @MethodSource("inValidPathsProvider")
+  void testInvalidSubDocumentPaths(String inValidPath) {
+    assertThrows(
+        IllegalArgumentException.class, () -> SubDocument.builder().path(inValidPath).build());
   }
 
   @Test
-  void testInValidSubDocumentPaths() {
-    String doubleColonsInBetweenInvalidPath = "price::quantity";
-    String startWithSpecialCharInvalidPath = "{hello_world";
-    String endWithSpecialCharInvalidPath = "hello_world}";
-    String createdTimeImplicitInvalidPath = "createdTime";
-    String lastUpdatedTimeImplicitInvalidPath = "lastUpdatedTime";
-    assertThrows(
-        IllegalArgumentException.class,
-        () -> SubDocument.builder().path(doubleColonsInBetweenInvalidPath).build());
-    assertThrows(
-        IllegalArgumentException.class,
-        () -> SubDocument.builder().path(startWithSpecialCharInvalidPath).build());
-    assertThrows(
-        IllegalArgumentException.class,
-        () -> SubDocument.builder().path(endWithSpecialCharInvalidPath).build());
-    assertThrows(
-        IllegalArgumentException.class,
-        () -> SubDocument.builder().path(createdTimeImplicitInvalidPath).build());
-    assertThrows(
-        IllegalArgumentException.class,
-        () -> SubDocument.builder().path(lastUpdatedTimeImplicitInvalidPath).build());
+  void testImplicitSubDocumentPaths() {
+    String createdTimeImplicitPath = "createdTime";
+    String lastUpdatedTimeImplicitPath = "lastUpdatedTime";
+    SubDocument implicitSubDocumentForCreatedTime = SubDocument.implicitCreatedTime();
+    SubDocument implicitSubDocumentForUpdatedTime = SubDocument.implicitUpdatedTime();
+    assertEquals(createdTimeImplicitPath, implicitSubDocumentForCreatedTime.getPath());
+    assertEquals(lastUpdatedTimeImplicitPath, implicitSubDocumentForUpdatedTime.getPath());
   }
 }
