601fb0196d0d62bf7948f571059afbec7fa4c473
avinash <avinash@traceable.ai>
2023-06-24 10:51:15 +0530

    Using toml in document-store

--- document-store/build.gradle.kts
+++ document-store/build.gradle.kts
@@ -7,28 +7,28 @@ plugins {
 }
 
 dependencies {
-  api("com.typesafe:config:1.4.2")
-  annotationProcessor("org.projectlombok:lombok:1.18.26")
-  compileOnly("org.projectlombok:lombok:1.18.26")
-  implementation("org.apache.commons:commons-collections4:4.4")
-  implementation("org.postgresql:postgresql:42.5.4")
-  implementation("org.mongodb:mongodb-driver-sync:4.9.0")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.15.1")
-  implementation("org.slf4j:slf4j-api:1.7.36")
-  implementation("org.apache.commons:commons-lang3:3.12.0")
-  implementation("net.jodah:failsafe:2.4.4")
-  implementation("com.google.guava:guava:32.0.1-jre")
-  implementation("org.apache.commons:commons-dbcp2:2.9.0")
+  api(libs.com.typesafe.config)
+  annotationProcessor(libs.org.projectlombok.lombok)
+  compileOnly(libs.org.projectlombok.lombok)
+  implementation(libs.org.apache.commons.commons.collections4)
+  implementation(libs.org.postgresql)
+  implementation(libs.org.mongodb.mongodb.driver.sync)
+  implementation(libs.com.fasterxml.jackson.core.jackson.databind)
+  implementation(libs.org.slf4j.slf4j.api)
+  implementation(libs.org.apache.commons.commons.lang3)
+  implementation(libs.net.jodah.failsafe)
+  implementation(libs.com.google.guava)
+  implementation(libs.org.apache.commons.commons.dbcp2)
 
-  testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-  testImplementation("org.mockito:mockito-core:5.2.0")
-  testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.mockito:mockito-junit-jupiter:5.2.0")
+  testImplementation(libs.org.junit.jupiter.junit.jupiter)
+  testImplementation(libs.org.mockito.mockito.core)
+  testImplementation(libs.org.mockito.mockito.inline)
+  testImplementation(libs.org.mockito.mockito.junit.jupiter)
 
-  integrationTestImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-  integrationTestImplementation("com.github.java-json-tools:json-patch:1.13")
-  integrationTestImplementation("org.testcontainers:testcontainers:1.17.6")
-  integrationTestImplementation("org.testcontainers:junit-jupiter:1.17.6")
+  integrationTestImplementation(libs.org.junit.jupiter.junit.jupiter)
+  integrationTestImplementation(libs.com.github.java.json.tools.json.patch)
+  integrationTestImplementation(libs.org.testcontainers)
+  integrationTestImplementation(libs.org.testcontainers.junit.jupiter)
 }
 
 tasks.test {
