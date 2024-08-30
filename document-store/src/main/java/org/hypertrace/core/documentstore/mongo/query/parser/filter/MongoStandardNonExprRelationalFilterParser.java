dca0524b5c13f35c1b621580f77e958da67a8337
Sanket Mundra <sanket.mundra@traceable.ai>
2024-08-30 13:41:08 +0530

    add comment

--- document-store/src/main/java/org/hypertrace/core/documentstore/mongo/query/parser/filter/MongoStandardNonExprRelationalFilterParser.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/mongo/query/parser/filter/MongoStandardNonExprRelationalFilterParser.java
@@ -19,6 +19,9 @@ public class MongoStandardNonExprRelationalFilterParser implements MongoRelation
     final String parsedLhs = expression.getLhs().accept(context.lhsParser());
     final String operator = mapping.getOperator(expression.getOperator());
     final Object parsedRhs = expression.getRhs().accept(context.rhsParser());
+
+    // using HashMap instead of Map.of() as RHS value can be null
+    // but Map.of() doesn't support null values
     final Map<String, Object> operatorToRhsMap = new HashMap<>();
     operatorToRhsMap.put(operator, parsedRhs);
     return Map.of(parsedLhs, unmodifiableMap(operatorToRhsMap));
