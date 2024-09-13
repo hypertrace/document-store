b3ab9a0c32f23a8dc3b76efe87d8a226a81ef346
Suresh Prakash <suresh.prakash@traceable.ai>
2024-09-13 16:00:18 +0530

    Minor fixes

--- document-store/src/main/java/org/hypertrace/core/documentstore/model/options/DataFreshness.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/model/options/DataFreshness.java
@@ -1,5 +1,6 @@
 package org.hypertrace.core.documentstore.model.options;
 
+@SuppressWarnings("UnnecessarySemicolon")
 public enum DataFreshness {
   REAL_TIME_FRESHNESS,
   NEAR_REAL_TIME_FRESHNESS,
--- document-store/src/main/java/org/hypertrace/core/documentstore/mongo/query/MongoReadPreferenceMapper.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/mongo/query/MongoReadPreferenceMapper.java
@@ -1,5 +1,7 @@
 package org.hypertrace.core.documentstore.mongo.query;
 
+import static com.mongodb.ReadPreference.primary;
+import static com.mongodb.ReadPreference.secondaryPreferred;
 import static java.util.Map.entry;
 import static org.hypertrace.core.documentstore.model.options.DataFreshness.NEAR_REAL_TIME_FRESHNESS;
 import static org.hypertrace.core.documentstore.model.options.DataFreshness.REAL_TIME_FRESHNESS;
@@ -12,8 +14,8 @@ import org.hypertrace.core.documentstore.model.options.DataFreshness;
 public class MongoReadPreferenceMapper {
   private static final Map<DataFreshness, ReadPreference> DATA_FRESHNESS_TO_READ_PREFERENCE =
       Map.ofEntries(
-          entry(REAL_TIME_FRESHNESS, ReadPreference.primary()),
-          entry(NEAR_REAL_TIME_FRESHNESS, ReadPreference.secondary()));
+          entry(REAL_TIME_FRESHNESS, primary()),
+          entry(NEAR_REAL_TIME_FRESHNESS, secondaryPreferred()));
 
   public ReadPreference readPreferenceFor(final DataFreshness dataFreshness) {
     return Optional.ofNullable(DATA_FRESHNESS_TO_READ_PREFERENCE.get(dataFreshness))
