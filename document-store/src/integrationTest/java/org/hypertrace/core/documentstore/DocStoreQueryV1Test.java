38d3b6c61f142afb78a6ec6fd6154530c09a929e
Suresh Prakash <suresh.prakash@traceable.ai>
2024-09-17 12:20:50 +0530

    Upgrade MongoDB version in integration tests

--- document-store/src/integrationTest/java/org/hypertrace/core/documentstore/DocStoreQueryV1Test.java
+++ document-store/src/integrationTest/java/org/hypertrace/core/documentstore/DocStoreQueryV1Test.java
@@ -132,7 +132,7 @@ public class DocStoreQueryV1Test {
   public static void init() throws IOException {
     datastoreMap = Maps.newHashMap();
     mongo =
-        new GenericContainer<>(DockerImageName.parse("mongo:4.4.0"))
+        new GenericContainer<>(DockerImageName.parse("mongo:7.0.14"))
             .withExposedPorts(27017)
             .waitingFor(Wait.forListeningPort());
     mongo.start();
