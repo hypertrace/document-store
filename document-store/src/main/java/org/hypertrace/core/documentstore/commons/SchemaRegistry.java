a62fbc218a6b3a68fd8dbd2d49e6a4b07d5ed39c
Prashant Pandey <prashant.pandey@harness.io>
2025-12-29 15:06:01 +0530

    Remove unused method in SchemaRegistry

--- document-store/src/main/java/org/hypertrace/core/documentstore/commons/SchemaRegistry.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/commons/SchemaRegistry.java
@@ -7,7 +7,5 @@ public interface SchemaRegistry<T extends ColumnMetadata> {
 
   Map<String, T> getSchema(String tableName);
 
-  void invalidate(String tableName);
-
   Optional<T> getColumnOrRefresh(String tableName, String colName);
 }
