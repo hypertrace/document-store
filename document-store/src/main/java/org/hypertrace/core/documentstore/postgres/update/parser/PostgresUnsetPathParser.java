e9537105469441d703757486090440090342ae41
Prashant Pandey <prashant.pandey@harness.io>
2026-03-16 14:33:59 +0530

    Always unset to NULL

--- document-store/src/main/java/org/hypertrace/core/documentstore/postgres/update/parser/PostgresUnsetPathParser.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/postgres/update/parser/PostgresUnsetPathParser.java
@@ -3,24 +3,11 @@ package org.hypertrace.core.documentstore.postgres.update.parser;
 import static org.hypertrace.core.documentstore.model.subdoc.SubDocument.PATH_SEPARATOR;
 import static org.hypertrace.core.documentstore.postgres.utils.PostgresUtils.formatSubDocPath;
 
-import org.hypertrace.core.documentstore.postgres.query.v1.parser.filter.nonjson.field.PostgresDataType;
-
 public class PostgresUnsetPathParser implements PostgresUpdateOperationParser {
 
   @Override
   public String parseNonJsonbField(final UpdateParserInput input) {
-    String baseField = input.getBaseField();
-
-    if (input.isArray()) {
-      // Array columns → empty array
-      return String.format("\"%s\" = '{}'", baseField);
-    } else if (input.getColumnType() == PostgresDataType.JSONB) {
-      // JSONB columns → empty object
-      return String.format("\"%s\" = '{}'::jsonb", baseField);
-    } else {
-      // Other columns → NULL
-      return String.format("\"%s\" = NULL", baseField);
-    }
+    return String.format("\"%s\" = NULL", input.getBaseField());
   }
 
   @Override
