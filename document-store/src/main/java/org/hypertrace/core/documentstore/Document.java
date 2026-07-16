d85626e061d386100cbb201218f548c5d71881fc
Prashant Pandey <prashant.pandey@harness.io>
2026-07-16 14:36:52 +0530

    Return JsonNode directly in JSONDocument

--- document-store/src/main/java/org/hypertrace/core/documentstore/Document.java
+++ document-store/src/main/java/org/hypertrace/core/documentstore/Document.java
@@ -1,8 +1,14 @@
 package org.hypertrace.core.documentstore;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 public interface Document {
 
   String toJson();
+
+  default JsonNode getJsonNode() {
+    return null;
+  }
 
   default DocumentType getDocumentType() {
     return DocumentType.NESTED;
